e1d0e2601f77f22a4854aee24fcaa16837529cb3
sunpeifu <sunpeifu@shuidi-inc.com>
2021-06-14 12:48:03 +0800

    haha

--- success/src/main/java/com/sunpeifu/success/SuccessApplication.java
+++ success/src/main/java/com/sunpeifu/success/SuccessApplication.java
@@ -6,12 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SuccessApplication {
 
-    //
+    // hha
     public static void main(String[] args) {
-
-
-        System.out.println("添加打印");
-        System.out.println("添加打印");
         SpringApplication.run(SuccessApplication.class, args);
     }
 
